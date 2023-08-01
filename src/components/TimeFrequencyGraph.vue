afc2c6c6c31d9e2afaa2ba1f51d8dbbcddddc845
qqqys <2636775389@qq.com>
2023-08-02 00:38:47 +0800

    feat: update time-frequency-graph with srp

--- src/components/TimeFrequencyGraph.vue
+++ src/components/TimeFrequencyGraph.vue
@@ -8,53 +8,70 @@
 <script setup lang="ts">
 import * as d3 from "d3";
 import { onMounted } from "vue";
-const width = 1000;
-const height = 400;
+import data from "../data"
+
+//全局变量
+const width = 1200;
+const p = 16
+const height = p*data.times.length;
+const freqSlice = 100;
 const margin = {
-    top: 0,
-    bottom: 30,
+    top: 30,
+    bottom: 0,
     left: 30,
-    right: 30,
+    right: 0,
 };
+const color = d3.scaleSequentialSqrt([0, d3.max(data.values, d => d3.max(d))], d3.interpolatePuRd)
+// const datas = {
+//     times: [],
+//     frequencys: [],
+//     values: []
+// }
+// for(let i=0; i<100; i++){
+//     datas.times.push(new Date().getTime()+i*1000)
+// }
+// for(let i=0; i<=freqSlice; i++){
+//     datas.frequencys.push(11950+i*(12000-11950)/freqSlice)
+// }
+// for(let i=0; i<100; i++){
+//     let tmp = []
+//     for(let j=0; j<=freqSlice; j++){
+//         tmp.push(Math.random()*50)
+//     }
+//     datas.values.push(tmp)
+// }
+// console.log(JSON.stringify(datas));
+
+
 onMounted(() => {
     const svg = d3
         .select("#time-frequency-view")
         .append("svg")
         .attr("width", width)
         .attr("height", height);
-    const data = {
-        times: [],
-        frequencys: [],
-        values: [],
-    };
-    for (let i = 1; i <= 100; i++) {
-        data.times.push(i)
-    }
-    for (let j = 1; j <= 1000; j++) {
-        data.frequencys.push(j)
-    }
-    for (let i = 1; i <= 100; i++) {
-        let tmp = []
-        for (let j = 1; j <= 1000; j++) {
-            tmp.push(Math.random() * 100)
-        }
-        data.values.push(tmp)
-    }
-    console.log(data);
+
 
     const y = d3
         .scaleBand()
         .domain(data.times)
         .rangeRound([margin.top, margin.top + height]);
+    const slice = (12000-11950)/freqSlice/2
     const x = d3
         .scaleLinear()
-        .domain([d3.min(data.frequencys), d3.max(data.frequencys) + 1])
-        .rangeRound([margin.left, width - margin.right]);
+        .domain([d3.min(data.frequencys)-slice,d3.max(data.frequencys)+slice])
+        .rangeRound([margin.left, width-margin.right]);
     svg
         .append("g")
         .attr("class", "y-axis")
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(d3.axisLeft(y));
+    svg
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${margin.top})`)
+        .call(d3.axisTop(x));
+    console.log(x(11950));
+    
     svg
         .append("g")
         .selectAll("g")
@@ -66,14 +83,19 @@ onMounted(() => {
         .selectAll("rect")
         .data((d) => d)
         .join("rect")
-        .attr("x", (d, i) => x(data.frequencys[i]) + 1)
+        .attr("x", (d, i) => x(data.frequencys[i])-1200/freqSlice/2)
         .attr(
             "width",
-            (d, i) => x(data.frequencys[i] + 1) - x(data.frequencys[i]) - 1
+            (d, i) => 1200/freqSlice
         )
         .attr("height", y.bandwidth() - 1)
-        .attr("fill", "#eee");
+        .attr("fill", d => isNaN(d) ? "#eee" : d === 0 ? "#fff" : color(d))
 });
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+#time-frequency-view{
+    max-height: 400px;
+    overflow-y: scroll;
+}
+</style>
