ac5e5d4c9c14450058134096eef54eb258d48855
qqqys <2636775389@qq.com>
2023-08-02 22:19:48 +0800

    feat: update global style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <TimeFrequencyGraph></TimeFrequencyGraph>
+  <TimeFrequencyGraph @timeSelect="handleTimeSelect"></TimeFrequencyGraph>
+  <Spectrogram ref="spectrogramRef"></Spectrogram>
 </template>
 
 <script setup lang="ts">
-import TimeFrequencyGraph from './components/TimeFrequencyGraph.vue';
-
+import TimeFrequencyGraph from "./components/TimeFrequencyGraph.vue";
+import Spectrogram from "./components/Spectrogram.vue";
+import { onMounted, ref } from "vue";
+const spectrogramRef = ref()
+const handleTimeSelect = (val: any) => {
+  spectrogramRef.value.draw(val)
+};
+onMounted(()=>{
+  console.log();
+})
 </script>
 
 <style scoped lang="less"></style>
